f3aba09f58292f7b6665c1458fa4f1080eb56140
Yusuf Kibaya <yusuf.kibaya@gmail.com>
2017-06-01 23:14:11 +0300

    Bare minimum boilerplate done

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ var poststylus = require('poststylus');
 var webpack = require('webpack');
 var rupture = require('rupture');
 var lost = require('lost');
+var cssnano = require('cssnano');
+var autoprefixer = require('autoprefixer');
+var postcss-font-magician = require('postcss-font-magician');
 
 module.exports = {
         entry: './src/index.js',
@@ -22,7 +25,11 @@ module.exports = {
 			      loader: 'postcss-loader',
 			      options: {
 			        plugins: [
-			          lost,
+			          lost, 
+			          /*cssnano,*/ 
+			          autoprefixer,
+			          postcss-font-magician
+
 			        ]
 			      }
 			    },
@@ -32,27 +39,30 @@ module.exports = {
 		    ]
 		  },
 		plugins: [
-		new webpack.LoaderOptionsPlugin({
-    test: /\.styl$/,
-    stylus: {
-      // You can have multiple stylus configs with other names and use them
-      // with `stylus-loader?config=otherConfig`.
-      default: {
-        use: [rupture()],
-      },
-    },
-  }),
+			new webpack.LoaderOptionsPlugin({
+				test: /\.styl$/,
+				stylus: {
+					// You can have multiple stylus configs with other names and use them
+					// with `stylus-loader?config=otherConfig`.
+					default: {
+					use: [rupture()],
+					},
+				},
+			}),
 			new HtmlWebpackPlugin({
 				title: 'Homepage',
-				filename: 'index.html'
+				filename: 'index.html',
+				template: './src/index.html'
 			}),
 			new HtmlWebpackPlugin({
 				title: 'Category',
-				filename: 'category.html'
+				filename: 'category.html',
+				template: './src/category.html'
 			}),
 			new HtmlWebpackPlugin({
 				title: 'Article',
-				filename: 'article.html'
+				filename: 'article.html',
+				template: './src/article.html'
 			}),
 		]
 }
